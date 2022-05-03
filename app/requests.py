2d52d7ef3ead605a39484e461612b16137d546bb
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-03 18:44:21 +0300

    getting sources

--- app/requests.py
+++ app/requests.py
@@ -12,3 +12,33 @@ def configure_request(app):
     apiKey = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
     article_url = app.config['NEWS_ARTICLE_URL']
+
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_source_url = base_url.format(category,apiKey)
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data = url.read()
+        get_source_response = json.loads(get_source_data)
+        source_results = None
+        if get_source_response['sources']:
+            source_results_list = get_source_response['sources']
+            source_results = process_results(source_results_list)
+    return source_results
+
+def process_results(source_list):
+    
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+   
+        source_object = Source(id,name,description,url,category)
+        source_results.append(source_object)
+
+    return source_results
