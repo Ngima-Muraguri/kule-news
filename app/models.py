fab73023847d4b258ebc950ddb994b7d9b24f035
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-03 18:41:07 +0300

    created class articles

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Articles:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
