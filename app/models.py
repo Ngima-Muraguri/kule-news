5ad601a201c1ffa30d14e44b8883a7b4ec4702d3
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-03 18:41:47 +0300

    created class source

--- app/models.py
+++ app/models.py
@@ -11,3 +11,15 @@ class Articles:
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
         self.content = content
+
+        class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name,description,url,category):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
